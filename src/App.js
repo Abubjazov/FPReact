7d1e4e33cac6b0e62301237281fa56e364609bf0
Radik Abubjazov <abubjazov@mail.ru>
2021-09-30 23:20:07 +0300

    Project: files preparing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-// import './App.css';
+// import './App.css'
+import React from 'react'
+import Layout from './hoc/Layout/Layout'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div style={{width: 400, border: '1px solid black'}}>
+          <h1>Layout</h1>
+        </div>
+      </Layout>
+    )
+  }
 }
 
-export default App;
+export default App
