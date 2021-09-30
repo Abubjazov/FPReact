52d6fcfea120f2ea13ac40b2af39dd7c14cbdc43
Radik Abubjazov <abubjazov@mail.ru>
2021-09-30 23:58:20 +0300

    containers: +Quiz, components: +ActiveQuiz

---
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -0,0 +1,15 @@
+import React from "react"
+import './ActiveQuiz.css'
+
+const ActiveQuiz = props => (
+    <div className='ActiveQuiz'>
+        <p className='Question'>
+            <span>
+                <strong>1.</strong>&nbsp;Вопрос?
+            </span>
+            <small>1 из 13</small>
+        </p>
+    </div>
+)
+
+export default ActiveQuiz
