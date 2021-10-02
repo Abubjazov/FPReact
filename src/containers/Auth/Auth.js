57626e53e1c7d3fe6927db240228fc7b9c4607bb
Radik Abubjazov <abubjazov@mail.ru>
2021-10-02 21:42:33 +0300

    вёрстка станицы Auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,51 @@
 import React from 'react'
+import './Auth.css'
+import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 
 class Auth extends React.Component {
+    loginHandler = () => {
+
+    }
+
+    registrationHandler = () => {
+        
+    }
+
+    submitHandler = (event) => {
+        event.preventDefault()
+    }
+
     render() { 
         return (
-        <div>
-            <h1>Auth</h1>
+        <div className='Auth'>
+            <div>
+                <h1>Авторизация</h1>
+
+                <form className='AuthForm' onSubmit={this.submitHandler}>
+                    
+                    <Input 
+                    label="Email"
+                    />
+                    <Input 
+                    label="Пароль"
+                    />
+
+                    <Button 
+                    type='success_btn' 
+                    onClick={this.loginHandler}
+                    >
+                    Войти
+                    </Button>
+                    
+                    <Button
+                    type='primary_btn' 
+                    onClick={this.registrationHandler}
+                    >
+                    Регистрация
+                    </Button>
+                </form>
+            </div>
         </div>
         )
     }
