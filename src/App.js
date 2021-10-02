379e98308b9b24ddf7bfd039050dacba51f1578a
Radik Abubjazov <abubjazov@mail.ru>
2021-10-02 19:04:39 +0300

    + роуты для страниц

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@
 import React from 'react'
 import Layout from './hoc/Layout/Layout'
 import Quiz from './containers/Quiz/Quiz'
+import {Route, Switch} from 'react-router-dom'
 
 class App extends React.Component {
   render() {
     return (
       <Layout>
-        <Quiz />
+        <Switch>
+          <Route path='/auth' component={Quiz}/>
+          <Route path='/quiz-creator' component={Quiz}/>
+          <Route path='/quiz/:id' component={Quiz}/>
+          <Route path='/' component={Quiz}/>
+        </Switch>
       </Layout>
     )
   }
